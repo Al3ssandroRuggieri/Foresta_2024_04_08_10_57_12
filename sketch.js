6f0a631f998a1a7d5f109f4fee5b74a4f4661f48
your name <your_email@example.com>
2024-04-08 13:10:29 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas3D(600, 400);
+  //Change background color of the scene
+
+  environment(SUNSET);
+  background3D("white"); 
+
+  let albero = beginGroup();
+  diffuse("green");
+  // sphere(0, 3, 0, 2);
+  align(TOP);
+  capsule(0, 2, 0, 2.5,2);
+  diffuse("brown");
+  cylinder(0, 0, 0, 0.8, 2.5);
+  endGroup();
+  
+  let lato = 100;
+  align(BOTTOM);
+  diffuse("#27800A")
+  box(0,0,0,lato,1,lato);
+  
+  for (let i = 0; i < 200; i++) {
+    let x = random(-lato/2,lato/2);
+    let z = random(-lato/2,lato/2);
+    let nuovoAlbero = clone(albero,x,0,z);       
+    let scala = random(0.5,2);
+    nuovoAlbero.setScale(scala);
+  }
+
+  
+  
+}
+
+function draw() {
+
+}
